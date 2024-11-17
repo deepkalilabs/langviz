0543bfcfc14d0b40051bfc8a10f6859da6a26442
Charles Javelona <charlesjavelona@Charless-MacBook-Pro.local>
2024-11-17 16:36:53 -0500

    Added frontend build hook

---
+++ frontend/scripts/setup-hooks.sh
@@ -0,0 +1,12 @@
+cat > scripts/setup-hooks.sh << 'EOL'
+#!/bin/sh
+
+# Get the git root directory
+GIT_ROOT=$(git rev-parse --git-dir)
+
+# Create symbolic link from .git/hooks to our .hooks directory
+ln -sf "../.hooks/pre-commit" "$GIT_ROOT/hooks/pre-commit"
+chmod +x ../.hooks/pre-commit
+
+echo "Git hooks installed succesfully!"
+EOL
